2662775c6b6042d9b66e00ca439f17ded3b1bbea
Roman <neirity@gmail.com>
2026-02-25 18:31:39 +0200

    fix: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
